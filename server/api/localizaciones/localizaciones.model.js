8b6f66c0d4db29a06930d46c15e3b55e3a84f8fd
Enrique Aparicio <enrique.ac9@gmail.com>
2015-04-15 18:10:00 +0100

    Built subexpuesta-v2 from commit 241dbe8 on branch master

--- server/api/localizaciones/localizaciones.model.js
+++ server/api/localizaciones/localizaciones.model.js
@@ -4,9 +4,21 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var LocalizacionesSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  titulo: String,
+  fechaSubida: {type: Date, default: Date.now},
+  fechaToma: {type: Date, default: Date.now},
+  autor: String,
+  votos: {type: Number, default: 0},
+  cloudinaryId: String,
+  tags: [String],
+  latitud: Number,
+  longitud: Number,
+  lugarPublico: Boolean,
+  acceso: String,
+  facilidadAcceso: Number,
+  contaminacionLuminica: Number,
+  notasAdicionales: String,
+  estado: {type: Number, default: 0} // 0->Pendiente de revisar
 });
 
 module.exports = mongoose.model('Localizaciones', LocalizacionesSchema);
